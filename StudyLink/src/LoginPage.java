1fbaed83dcd01359cd4c958718d973af304c7e3b
Ammiel Cruz <aemc@Ammiels-MacBook-Pro-2.local>
2023-02-04 17:26:10 -0500

    add gui skeleton

--- StudyLink/src/LoginPage.java
+++ StudyLink/src/LoginPage.java
@@ -1,7 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Login() {
+public class LoginPage extends JFrame implements ActionListener {
+    public LoginPage() {
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
--- StudyLink/src/Main.java
+++ StudyLink/src/Main.java
@@ -1,8 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class Main {
     public static void main(String[] args) {
         try {
             //create instance of the LoginPage
-            CreateLoginForm login = new LoginPage();
+            new GUI();
         }
         catch(Exception e) {
             //handle exception
