eaece1eff86759ff1f874c22280eb25371bbad0a
Ammiel Cruz <aemc@Ammiels-MacBook-Pro-2.local>
2023-02-05 18:52:48 -0500

    add login page components

--- StudyLink/src/LoginPage.java
+++ StudyLink/src/LoginPage.java
@@ -1,15 +1,44 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class LoginPage extends JFrame implements ActionListener {
+public class LoginPage {
+
+    private JLabel userLabel;
+    private JTextField userText;
+    private JLabel passwordLabel;
+    private JTextField passwordText;
     public LoginPage() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        frame.setSize(500, 500);
+        frame.add(panel);
 
-    }
+        panel.setLayout(null);
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+        userLabel = new JLabel("User");
+        userLabel.setBounds(10, 30, 80, 25);
+        panel.add(userLabel);
 
+        userText = new JTextField(20);
+        userText.setBounds(100, 30, 165, 25);
+        panel.add(userText);
+
+        passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 60, 80, 25);
+        panel.add(passwordLabel);
+
+        passwordText = new JTextField(20);
+        passwordText.setBounds(100, 60, 165, 25);
+        panel.add(passwordText);
+
+        JButton loginBtn = new JButton("Login");
+        loginBtn.setBounds(120, 100, 80, 25);
+        panel.add(loginBtn);
+
+        JLabel success = new JLabel("");
+        success.setBounds(10, 110, 300, 25);
+        panel.add(success);
+
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
--- StudyLink/src/Main.java
+++ StudyLink/src/Main.java
@@ -1,15 +1,6 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 public class Main {
+
     public static void main(String[] args) {
-        try {
-            //create instance of the LoginPage
-            new GUI();
-        }
-        catch(Exception e) {
-            //handle exception
-        }
+        new LoginPage();
     }
 }
