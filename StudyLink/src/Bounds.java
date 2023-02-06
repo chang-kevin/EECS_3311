4ef5b337367ee2b04dd55f5dcff9d3f71cea5e44
Ammiel <aemc@users.noreply.github.com>
2023-02-05 21:16:07 -0500

    refactor login page (#1)
    Co-authored-by: Ammiel Cruz <aemc@Ammiels-MacBook-Pro-2.local>

---
+++ StudyLink/src/Bounds.java
@@ -0,0 +1,29 @@
+public class Bounds {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    public Bounds(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
--- StudyLink/src/LoginPage.java
+++ StudyLink/src/LoginPage.java
@@ -1,44 +1,84 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class LoginPage {
+/**
+ * Login Page component responsible for authenticating the user and directing them to the
+ * Dashboard Page.
+ */
+public class LoginPage implements ActionListener {
 
     private JLabel userLabel;
     private JTextField userText;
     private JLabel passwordLabel;
     private JTextField passwordText;
+    private JLabel success;
+
+    private JButton loginBtn;
+
+    JPanel panel;
     public LoginPage() {
         JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
+        panel = new JPanel();
         frame.setSize(500, 500);
         frame.add(panel);
 
         panel.setLayout(null);
 
-        userLabel = new JLabel("User");
-        userLabel.setBounds(10, 30, 80, 25);
+        Bounds userLabelBounds = new Bounds(10, 30, 80, 25);
+        userLabel = this.generateLabelComponent("User", userLabelBounds);
         panel.add(userLabel);
 
-        userText = new JTextField(20);
-        userText.setBounds(100, 30, 165, 25);
+        Bounds userTextBounds = new Bounds(100, 30, 165, 25);
+        userText = this.generateFieldComponent(userTextBounds);
         panel.add(userText);
 
-        passwordLabel = new JLabel("Password");
-        passwordLabel.setBounds(10, 60, 80, 25);
+        Bounds passwordLabelBounds = new Bounds(10, 60, 80, 25);
+        passwordLabel = this.generateLabelComponent("Password", passwordLabelBounds);
         panel.add(passwordLabel);
 
-        passwordText = new JTextField(20);
-        passwordText.setBounds(100, 60, 165, 25);
+        Bounds passwordFieldBounds = new Bounds(100, 60, 165, 25);
+        passwordText = this.generateFieldComponent(passwordFieldBounds);
         panel.add(passwordText);
 
-        JButton loginBtn = new JButton("Login");
+        loginBtn = new JButton("Login");
         loginBtn.setBounds(120, 100, 80, 25);
+        loginBtn.addActionListener(this);
         panel.add(loginBtn);
 
-        JLabel success = new JLabel("");
+        success = new JLabel("");
         success.setBounds(10, 110, 300, 25);
         panel.add(success);
 
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    /**
+     * Generate Label component
+     * @param componentLabel
+     * @param bounds
+     * @return
+     */
+    private JLabel generateLabelComponent(String componentLabel, Bounds bounds) {
+        JLabel label = new JLabel(componentLabel);
+        label.setBounds(bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight());
+        return label;
+    }
+
+    /**
+     * Generate JField component
+     * @param bounds
+     * @return
+     */
+    private JTextField generateFieldComponent(Bounds bounds) {
+        JTextField text = new JTextField(20);
+        text.setBounds(bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight());
+        return text;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        success.setText("Login successful!");
     }
 }
