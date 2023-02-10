77ff7d348da957eda4e7d9a4f02bf8ecc93b0ac8
YuvteshMann <87862238+YuvteshMann@users.noreply.github.com>
2023-02-09 23:00:30 -0500

    Update CourseList.java

--- StudyLink/src/domainobjects/CourseList.java
+++ StudyLink/src/domainobjects/CourseList.java
@@ -1,15 +1,15 @@
 
-        package domainobjects;
+      
+
+
+    package domainobjects;
 import java.util.*;
 
 public class CourseList {
-    private ArrayList<Course> courses = new ArrayList<Course>();
-
-    public CourseList(ArrayList<Course> courses) {
-        this.courses = courses;
-    }
-
-    public CourseList(){}
+    public static ArrayList<Course> courses = new ArrayList<Course>();
+    public static final CourseList courselist = new CourseList();
+    
+    private CourseList(){}
 
     public ArrayList<Course> getCourses() {
         return courses;
@@ -23,35 +23,45 @@ public class CourseList {
         courses.add(course);
     }
 
-    public  void search(String coursenumber) {
+    public static void search(int coursenumber) {
 
         for (Iterator<Course> i = courses.iterator();
              i.hasNext(); ) {
             Course course = i.next();
-            if (course.getCoursenumber().matches(coursenumber) ){
-                System.out.println(course.toString());
+            if (course.getCoursenumber() == coursenumber ){
+                System.out.print("Result of the search :" );
+                course.info();
             } }
 
     }
-    public  void search( String coursename, int yearlevel) {
+    public static  void searchbyname( String coursename) {
 
         for (Iterator<Course> i = courses.iterator();
              i.hasNext(); ) {
             Course course = i.next();
-            if (course.getCoursenumber().matches(coursename) ){
-                System.out.println(course.toString());
+            if (course.getCoursename().matches(coursename) ){
+                System.out.print("Result of the search:" );
+                course.info();
             } }
 
     }
-    public void search( int yearlevel) {
+    public static void list( int yearlevel) {
         ArrayList<Course> matchingcourses = new ArrayList<Course>();
         for (Iterator<Course> i = courses.iterator();
              i.hasNext(); ) {
             Course course = i.next();
             if (course.getYearlevel() == yearlevel )
                 matchingcourses.add(course); }
+        System.out.println("List of all courses in " + yearlevel + "year");
         for(int i=0;i<matchingcourses.size();i++){
-            System.out.println(matchingcourses.get(i).toString());
+            matchingcourses.get(i).info();
+        }
+
+    }
+    public static void list(){
+        System.out.println("List of  eecs courses");
+        for (int j = 0;j<courses.size();j++){
+            courses.get(j).info();
         }
 
     }
