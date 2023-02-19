9dab180993746fcf05cb9eae79f161faf5f4e3cb
Ammiel <aemc@users.noreply.github.com>
2023-02-19 16:35:49 -0500

    add course list feature (#73)
    Co-authored-by: Ammiel Cruz <aemc@Ammiels-MacBook-Pro-2.local>

--- StudyLink/src/controller/Main.java
+++ StudyLink/src/controller/Main.java
@@ -6,7 +6,17 @@ import javax.swing.*;
 
 public class Main extends JFrame {
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createGUI();
+            }
+        });
+    }
+
+    public static void createGUI() {
         new Login();
-    }}
+    }
+}
 
 
--- StudyLink/src/view/CourseList.java
+++ StudyLink/src/view/CourseList.java
@@ -1,36 +1,55 @@
 package view;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
 
 public class CourseList extends JFrame {
-    private JTable table;
+    private String[] columnNames = { "Department", "Course Number", "Description" };
     private JPanel panel1;
+    private JTable courseList;
+    private JComboBox sortComboBox;
+    private JButton button1;
+    private JButton button2;
 
     public CourseList() {
+        createTable();
+        createCourseCombo();
+    }
+
+    private void createTable() {
         setVisible(true);
         setTitle("Course List");
         setContentPane(panel1);
+        pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 800, 600);
         setLocationRelativeTo(null);
 
-        String[] columnNames = { "Department", "Course Number", "Description", "Session" };
-
-        // Data to be displayed in the JTable
+        // remove when connected to DB
+        String[] columnNames = { "Department", "Course Number", "Description" };
         String[][] data = {
-                { "EECS", "3311", "Software Design", "W" },
-                { "EECS", "3101", "Introduction to Algorithms", "W" }
+                { "EECS", "3311", "Software Design" },
+                { "EECS", "3101", "Introduction to Algorithms" }
         };
 
-        DefaultTableModel model = new DefaultTableModel();
-        for (String columnName: columnNames) {
-            model.addColumn(columnName);
-        }
+        DefaultTableModel model = new DefaultTableModel(
+                data,
+                columnNames
+        );
+        courseList.setModel(model);
+        TableColumnModel columns = courseList.getColumnModel();
+        columns.getColumn(0).setMinWidth(100);
+        columns.getColumn(1).setMinWidth(100);
 
-        model.addRow(new Object[]{"EECS", "3311", "Software Design", "W"});
-        model.addRow(new Object[]{ "EECS", "3101", "Introduction to Algorithms", "W" });
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment(JLabel.CENTER);
+        columns.getColumn(0).setCellRenderer(centerRenderer);
+        columns.getColumn(1).setCellRenderer(centerRenderer);
+    }
 
-        table.setModel(model);
+    private void createCourseCombo() {
+        sortComboBox.setModel(new DefaultComboBoxModel(columnNames));
     }
 }
