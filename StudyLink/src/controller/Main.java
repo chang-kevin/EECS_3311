467489c59ebdaf99f8d1c2367227221af362fc4f
Kevin Chang <kev10th@my.yorku.ca>
2023-02-24 17:52:22 -0500

    connected the database to our application

--- StudyLink/src/controller/Main.java
+++ StudyLink/src/controller/Main.java
@@ -1,17 +1,52 @@
 package controller;
 
+import java.sql.Connection;
+
 import view.Login;
+
+import java.sql.Driver;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import javax.swing.*;
 
 public class Main extends JFrame {
+    
     public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/studylink";
+        String username = "root";
+        String password = "kungfuhustle10";
+        Connection connection;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+    
+            connection = DriverManager.getConnection(url, username, password);
+
+            Statement statement = connection.createStatement(); //allows SQL to be executed
+
+            ResultSet result = statement.executeQuery("select * from courses");
+
+            while (result.next() != false) {
+                System.out.println(result.getString("course_name"));
+            }
+
+        }
+    
+        catch (Exception e) {
+            System.out.println(e);
+        }
+
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 createGUI();
             }
         });
+
     }
 
     public static void createGUI() {
