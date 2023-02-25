04da176ace7fc8ea3481b90a60d0c3e863f4b819
Ammiel Cruz <aemc@Ammiels-MacBook-Pro-2.local>
2023-02-25 16:03:02 -0500

    add email + first + last name fields

--- StudyLink/src/view/AccountManagement.java
+++ StudyLink/src/view/AccountManagement.java
@@ -1,13 +1,54 @@
 package view;
 
-import javax.swing.*;
+import view.dashboard.Dashboard;
 
-public class AccountManagement {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AccountManagement extends JFrame {
     private JPanel panel1;
-    private JButton button1;
-    private JButton button2;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
+    private JButton cancelButton;
+    private JButton saveButton;
+    private JTextField emailText;
+    private JTextField firstNameText;
+    private JTextField lastNameText;
+    private JLabel email;
+    private JLabel firstName;
+    private JLabel lastName;
+
+    public AccountManagement() {
+        setVisible(true);
+        setTitle("StudyLink");
+        setContentPane(panel1);
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 800, 600);
+        setLocationRelativeTo(null);
+
+        setUpCancelBtn();
+        setUpSaveBtn();
+    }
+
+    private void setUpCancelBtn() {
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                dispose();
+                new Dashboard();
+            }
+        });
+    }
+
+    private void setUpSaveBtn() {
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                dispose();
+                new Dashboard();
+            }
+        });
+    }
 }
