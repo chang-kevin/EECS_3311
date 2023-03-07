d2838b5a9e58885827e95142cc06fb13232fe02c
manalo-angela <77253494+manalo-angela@users.noreply.github.com>
2023-03-07 02:54:19 -0500

    CardLayout Display
    Added the display panels using cardlayout , created a template for the course pages and connected the panels to the frame.

---
+++ StudyLink/src/view/dashboard/CardLayoutDisplay.java
@@ -0,0 +1,261 @@
+package view.dashboard;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.text.*;
+
+/**
+ * 
+ * Class to create the cardlayout for the dashboard display area. 
+ *
+ */
+
+public class CardLayoutDisplay extends JPanel implements ActionListener {
+
+	private CardLayout cardLayout;
+	JPanel displayArea; 
+	private JButton one;
+	private JButton two;
+	private JButton three;
+	private JButton courseBtn;
+	private JButton uploadBtn;
+	private JButton dashboardBtn;
+	JPanel taskbar;
+	private JPanel buttons;
+	
+	private static final String dashboardPanel = "Dashboard"; 
+	private static final String uploadPanel = "Upload"; 
+	private static final String onePanel = "One"; 
+	private static final String twoPanel = "Two";
+	private static final String threePanel = "Three"; 
+
+	/**
+	 * Constructor
+	 */
+	
+	public CardLayoutDisplay() {			
+		displayArea = new JPanel();
+		displayArea = panelBorder(displayArea);
+		
+		cardLayout = new CardLayout(0, 0);	
+		displayArea.setLayout(cardLayout);
+		
+		createTaskbarPanel();
+		createCard();
+		
+		
+	}
+	/**
+	 * This method creates an object of the display panel and adds it into the cardlayout.
+	 */
+	public void createCard() {
+		HomePage home = new HomePage();
+		CourseLevel courses = new CourseLevel(); 	
+		
+		addCard(home.dashboard, dashboardPanel);	
+		addCard(courses.oneLevel, onePanel);	
+		addCard(courses.twoLevel, twoPanel);
+		addCard(courses.threeLevel, threePanel);
+	}
+
+	/**
+	 * This method adds the panels in the cardlayout. 
+	 * @param page JPanel component to be added to the cardlayout.
+	 * @param key String key that identifies the component to be added. 
+	 */
+	public void addCard(JPanel page, String key) {
+		displayArea.add(page, key);
+	}
+	
+	
+	/**
+	 * This method sets the layout for the taskbar buttons. 
+	 * @param btn JButton component 
+	 * @param name Identifier name for the JButton
+	 * @param y y-bound for JButton location
+	 * @return JButton with custom layouts 
+	 */
+	public JButton createButton(JButton btn, String name, int y) {
+		btn = new JButton(name);
+		if(name == "Dashboard") {
+			btn.setBorder(new MatteBorder(0, 0, 0, 3, (Color) new Color(239, 127, 127)));
+			btn.setForeground(new Color(241, 171, 165));
+		}
+		else {
+			btn.setForeground(new Color(255, 255, 255));
+		}
+		btn.setFont(new Font("Microsoft JhengHei UI", Font.BOLD, 18));
+		btn.setRolloverEnabled(false);
+		btn.setBounds(0, y, 160, 40);
+		btn.setBorder(null);
+		btn.setOpaque(false);
+		btn.setBackground(new Color(216, 237, 214, 100));
+		btn.setFocusPainted(false);
+		btn.addActionListener(this);
+		taskbar.add(btn);
+
+		return btn;
+	}
+	/**
+	 * This method adds JButtons to a panel creating a drop down menu. 
+	 */
+	
+	public void levelMenu() {
+		one = new JButton("1000 Level");
+		two = new JButton("2000 Level");
+		three = new JButton("3000 Level");
+		JButton[] arr = {one, two, three};
+		buttons = new JPanel();
+		buttons.setBorder(null);
+		buttons.setBounds(0, 0, 0, 0);
+		buttons.setBackground(new Color(216, 237, 214));
+		buttons.setLayout(null);
+
+
+		int y = 0;
+		for(int i = 0; i < 3; i++) {
+			arr[i].setBackground(new Color(216, 237, 214));
+			arr[i].setForeground(new Color(255, 255, 255));
+			arr[i].setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 15));
+			arr[i].setBorder(null);
+			arr[i].setBorderPainted(false);
+			arr[i].addActionListener(this);
+			arr[i].setFocusPainted(false);
+			buttons.add(arr[i]);
+			arr[i].setBounds(0, y, 160, 40);
+			y = y + 40;
+		}
+
+		taskbar.add(buttons);
+
+
+	}
+	/**
+	 * This method creates the side panel containing the taskbar buttons and application name.
+	 * @return JPanel component for the taskbar 
+	 */
+	public JPanel createTaskbarPanel() {
+		taskbar = new JPanel();
+		taskbar.setBackground(new Color(216, 237, 214));
+		taskbar.setBounds(0, 0, 160, 561);
+		taskbar.setLayout(null);
+
+		JTextPane studylink = new JTextPane();
+		studylink.setMargin(new Insets(5, 10, 5, 10));
+		studylink.setFont(new Font("Microsoft JhengHei", Font.BOLD, 20));
+		StyledDocument style = studylink.getStyledDocument();
+		javax.swing.text.Style study = studylink.addStyle("style", null);
+		StyleConstants.setForeground(study, new Color(115, 165, 128));
+		try {
+			style.insertString(style.getLength(), "Study", study); }
+		catch(BadLocationException e) {}
+		StyleConstants.setForeground(study, new Color(239, 127, 127));
+		try {
+			style.insertString(style.getLength(), "Link", study); }
+		catch(BadLocationException e) {}
+
+
+		studylink.setBounds(32, 11, 101, 32);
+		studylink.getHighlighter().removeAllHighlights();
+		studylink.setEditable(false);
+		studylink.setBackground(new Color(216, 237, 214, 100));
+		studylink.setOpaque(false);
+		studylink.setBorder(null);
+		taskbar.add(studylink);
+
+		dashboardBtn = createButton(dashboardBtn, "Dashboard", 203);
+		courseBtn = createButton(courseBtn, "Course Levels", 243);
+		uploadBtn = createButton(uploadBtn, "Upload", 283);
+		levelMenu();
+
+		return taskbar;
+
+	}
+	/**
+	 * This method creates a rounded border and sets the layout for the panel display. 
+	 * @param panel parent JPanel of the cardlayout 
+	 * @return Returns the parent panel with rounded boarder. 
+	 */
+	 public JPanel panelBorder(JPanel panel) {
+	        panel = new JPanel() {
+	            @Override
+	            protected void paintComponent(Graphics g) {
+	                super.paintComponent(g);
+	                Dimension arcs = new Dimension(50, 50);
+	                int width = getWidth();
+	                int height = getHeight();
+	                Graphics2D graphics = (Graphics2D) g;
+	                graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+	                graphics.setColor(getBackground());
+	                graphics.fillRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height);
+	                graphics.setColor(getForeground());
+	                graphics.drawRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height);
+	            }
+	        };
+
+	        panel.setForeground(new Color(239, 239, 239, 75));
+	        panel.setBackground(new Color(239, 239, 239, 75));
+	        panel.setBounds(169, 203, 537, 348);
+	        panel.setBorder(null);
+	        panel.setOpaque(false);
+	        panel.setLayout(null);
+
+
+	        return panel;
+	    }
+
+
+	 /**
+	  * Invoked on button click and displays a card. 
+	  */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton[] menu = {dashboardBtn, courseBtn, uploadBtn, one, two, three};
+
+		for(JButton btn: menu) {
+			if(e.getSource() == btn) {
+				((JComponent) e.getSource()).setBorder(new MatteBorder(0, 0, 0, 3, (Color) new Color(239, 127, 127)));
+				btn.setForeground(new Color(241, 171, 165));
+				if(btn == dashboardBtn) {
+					cardLayout.show(displayArea, dashboardPanel);
+					uploadBtn.setBounds(0, 283, 160, 40);
+					buttons.setBounds(0, 0, 0 ,0);
+
+				}
+				else if(btn == courseBtn) {
+					uploadBtn.setBounds(0, 403, 160, 40);
+					buttons.setBounds(0, 283, 160, 120);
+
+				}
+				else if(btn == one) {
+					cardLayout.show(displayArea, onePanel);
+					uploadBtn.setBounds(0, 403, 160, 40);
+					buttons.setBounds(0, 283, 160, 120);
+				}
+				else if(btn == two) {
+					cardLayout.show(displayArea, twoPanel);
+					uploadBtn.setBounds(0, 403, 160, 40);
+					buttons.setBounds(0, 283, 160, 120);
+				}
+				else if(btn == three) {
+					cardLayout.show(displayArea, threePanel);
+					uploadBtn.setBounds(0, 403, 160, 40);
+					buttons.setBounds(0, 283, 160, 120);
+				}
+				else {
+					uploadBtn.setBounds(0, 283, 160, 40);
+					buttons.setBounds(0, 0, 0 ,0);
+				}
+			}
+			else {
+				btn.setBorder(null);
+				btn.setForeground(new Color(255, 255, 255));
+			}
+		}
+		
+	}
+	
+	
+}
---
+++ StudyLink/src/view/dashboard/CourseLevel.java
@@ -0,0 +1,265 @@
+package view.dashboard;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+
+/**
+ * 
+ * Class for creating the course page by level and displays the courses under each one. 
+ * 
+ */
+public class CourseLevel extends JPanel implements ActionListener{
+    
+	JPanel twoLevel; 
+	JPanel threeLevel;
+	JPanel oneLevel;
+	JFrame frame;
+	private JPanel courseContentPanel;
+	private JPanel labelContainer;
+	private JButton bookmark;
+	private JButton view;
+
+    /*
+     * Constructor 
+     */
+    public CourseLevel(){
+		
+		oneLevel = new JPanel();
+		oneLevel = createCourseLevels(oneLevel, "1000 Level Courses");
+		
+		
+		twoLevel = new JPanel();
+		twoLevel = createCourseLevels(twoLevel, "2000 Level Courses");
+		
+		
+		threeLevel = new JPanel();
+		threeLevel = createCourseLevels(threeLevel, "3000 Level Courses");
+		
+		
+    	
+    }
+    
+    /**
+     * This method creates and sets layout the JPanel component for each course level.
+     * @param level JPanel component for a course level.
+     * @param title Page title for the course page. 
+     * @return Returns the panel for a course level page.
+     */
+    public JPanel createCourseLevels(JPanel level, String title) {
+		
+		level = panelBorder(level);
+		
+		JLabel pageTitle = new JLabel();
+		pageTitle.setForeground(new Color(241, 171, 165));
+		pageTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		pageTitle.setFont(new Font("Microsoft JhengHei UI", Font.BOLD, 15));
+		pageTitle.setText(title);
+		pageTitle.setBounds(10, 11, 142, 29);
+		level.add(pageTitle);
+		
+		courseContentPanel = new JPanel();
+		courseContentPanel = coursePanel(); 
+		level.add(courseContentPanel);
+			
+		
+		labelContainer = new JPanel();
+		labelContainer = createContainer(labelContainer, 0);
+		courseContentPanel.add(labelContainer);
+		
+		contentLabels(labelContainer);
+    
+		generateCourses();
+		
+		return level;
+    }
+    /**
+     * This method sets a layout for the JPanel container of each course to be displayed as a list. 
+     * @param container JPanel component containing the course information. 
+     * @param y-bound value for container placement. 
+     * @return Returns the container panel.
+     */
+    public JPanel createContainer(JPanel container, int y) {	
+    	container.setBackground(new Color(255, 255, 255));
+    	container.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(192, 192, 192)));
+    	container.setBounds(10, y, 497, 40);
+    	container.setLayout(null);
+		return container;
+    }
+    /**
+     * This method creates the information label for each list of courses per course page. 
+     * @param labelContainer JPanel container for the label for absolute label placement.
+     */
+    public void contentLabels(JPanel labelContainer) {
+    	JLabel courseCode = new JLabel("Course Code");
+		createLabels(courseCode, 0, 100, labelContainer);	
+		
+		JLabel courseName = new JLabel("Course Name");
+		createLabels(courseName, 100, 197, labelContainer);
+		
+		JLabel bookmarkLabel = new JLabel("Bookmark");
+		createLabels(bookmarkLabel, 297, 100, labelContainer); 	
+		
+		JLabel viewCourse = new JLabel("View Course");
+		createLabels(viewCourse, 397, 100, labelContainer);
+
+    }
+    
+    /**
+     * This method sets the layout for the labels. 
+     * @param label JLabel containing the label for each course information. 
+     * @param x x-bound value for placement.
+     * @param width The width of the label. 
+     * @param container JPanel container where the labels are to be added. 
+     */
+    public void createLabels(JLabel label, int x, int width, JPanel container) {
+    	label.setLayout(null);
+    	label.setForeground(new Color(150, 150, 150));
+    	label.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 12));
+    	label.setBounds(x, 0, width, 40);
+    	label.setHorizontalAlignment(SwingConstants.CENTER);
+    	container.add(label);
+    }
+    
+    /**
+     * This method sets the layout for the panel containing the list of the courses. 
+     * @return
+     */
+    public JPanel coursePanel() {
+		courseContentPanel.setBackground(new Color(255, 255, 255));
+		courseContentPanel.setBounds(10, 48, 517, 289);
+		courseContentPanel.setLayout(null);
+		
+		return courseContentPanel; 
+    }
+    
+    /**
+     * This method generates the courses and displays it on a list. 
+     */
+    public void generateCourses() {
+    	//y-value of panel used to hold the a course 
+    	int y = 40; 
+    	
+    	// get number of courses then iterate through this 
+    	
+    	//number of courses change value 
+    	int num = 2;
+    	String name = "name test";
+    	String code = "code 0000";
+    	
+//    	for(int i = 0; i < num; i++) {
+    		JPanel container = new JPanel(); 
+    		container = createContainer(container, y);
+    		courseContentPanel.add(container);  	
+    		createCourse(container, name, code);
+    		y = y + 40;
+    	//}
+    	
+    	
+    }
+    
+   
+    /**
+     * This method creates the components for the information on each courses (course code, name, bookmark, view course). 
+     * @param container JPanel container for absolute placement of the course components. 
+     * @param courseName Course name to be displayed. 
+     * @param courseCode Course code to be displayed. 
+     */
+    public void createCourse(JPanel container, String courseName, String courseCode) {
+    	
+    	JLabel code = new JLabel(courseCode);
+    	createLabels(code, 0, 100, container);
+    	
+    	JLabel name = new JLabel(courseName); 
+    	createLabels(name, 100, 197, container); 
+    	
+    	String state = "Bookmark";
+    	bookmark = new JButton(); 
+    	bookmark = buttonStyler(bookmark, state);
+    	container.add(bookmark);
+    	bookmark.setBounds(308, 7, 80, 25);
+    	
+    	String text = "View";
+    	view = new JButton();
+    	view = buttonStyler(view, text); 
+    	container.add(view);
+    	view.setBounds(408, 7, 80, 25);
+    	
+    }
+    
+    
+    /**
+     * This method sets the layout for the bookmark and view buttons. 
+     * @param button JButton component for the courses. 
+     * @param text Name to be displayed as text on the button. 
+     * @return Returns the customized JButton.
+     */
+    public JButton buttonStyler(JButton button, String text) {
+    	button.setText(text);
+		button.setFont(new Font("Dubai", Font.BOLD, 13));
+		button.setOpaque(true);
+		button.setBackground(new Color(216, 237, 214));
+		button.setForeground(new Color(255, 255, 255));
+		button.setFocusPainted(false);		
+		button.setBorder(new LineBorder(new Color(255, 255, 255), 2, true));
+		button.addActionListener(this);
+		return button;
+    	
+    }
+    
+    /**
+	 * This method creates a rounded border and sets the layout for each course level panel. 
+	 * @param panel JPanel for the course page 
+	 * @return Returns the course page panel with rounded border. 
+	 */
+    public JPanel panelBorder(JPanel level) {
+		level = new JPanel() {
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				Dimension arcs = new Dimension(50, 50);
+				int width = getWidth();
+				int height = getHeight();
+				Graphics2D graphics = (Graphics2D) g;
+				graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+				graphics.setColor(getBackground());
+				graphics.fillRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height);
+				graphics.setColor(getForeground());
+				graphics.drawRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height);
+			}
+		};
+
+		level.setForeground(new Color(207, 234, 204));
+		level.setOpaque(false);
+		level.setBackground(new Color(207, 234, 204));
+		level.setBounds(169, 203, 537, 348);		
+		level.setLayout(null);	
+
+
+		return level;
+	}
+    
+    
+    /**
+	  * Invoked on button click which changes the bookmark state. 
+	  */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == bookmark) {
+			if(bookmark.getText() == "Bookmark")
+			{
+				bookmark.setBackground(new Color(239, 163, 156));
+				bookmark.setText("Remove");
+			}
+			else {
+				bookmark.setText("Bookmark");
+				bookmark.setBackground(new Color(216, 237, 214));
+			}
+		}
+		
+	}
+    
+	
+}
