9f25efab292ea0c552d22155c9d01635e542dc44
Kevin Chang <kev10th@my.yorku.ca>
2023-03-14 14:18:49 -0400

    implemented FocusListener on the HintTextFiled class

--- StudyLink/src/view/dashboard/SearchBar.java
+++ StudyLink/src/view/dashboard/SearchBar.java
@@ -3,6 +3,8 @@ package view.dashboard;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.image.BufferedImage;
 
 
@@ -65,27 +67,37 @@ public class SearchBar extends JLabel {
     /**
      * Adds the input hint for the search bar prompt
      */
-    public class HintTextField extends JTextField {
-        public HintTextField(String hint) {
-            _hint = hint;
+    class HintTextField extends JTextField implements FocusListener {
+
+        private final String hint;
+        private boolean showingHint;
+
+        public HintTextField(final String hint) {
+            super(hint);
+            this.hint = hint;
+            this.showingHint = true;
+            super.addFocusListener(this);
         }
+
         @Override
-        public void paint(Graphics g) {
-            super.paint(g);
-            if (getText().length() == 0) {
-                int h = getHeight();
-                ((Graphics2D)g).setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-                Insets ins = getInsets();
-                FontMetrics fm = g.getFontMetrics();
-                int c0 = getBackground().getRGB();
-                int c1 = getForeground().getRGB();
-                int m = 0xfefefefe;
-                int c2 = ((c0 & m) >>> 1) + ((c1 & m) >>> 1);
-                g.setColor(new Color(c2, true));
-                g.drawString(_hint, ins.left, h / 2 + fm.getAscent() / 2 - 2);
+        public void focusGained(FocusEvent e) {
+            if(this.getText().isEmpty()) {
+                super.setText("");
+                showingHint = false;
             }
         }
-        private final String _hint;
+        @Override
+        public void focusLost(FocusEvent e) {
+            if(this.getText().isEmpty()) {
+                super.setText(hint);
+                showingHint = true;
+            }
+        }
+
+        @Override
+        public String getText() {
+            return showingHint ? "" : super.getText();
+        }
     }
 
 
