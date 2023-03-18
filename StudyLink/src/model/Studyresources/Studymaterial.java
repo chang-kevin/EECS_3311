e5302cb32bb528d96059998d93ffed49483fc8ed
YuvteshMann <mann.yuvtesh@gmail.com>
2023-03-18 18:32:18 -0400

    added Studymaterial

--- StudyLink/src/model/Studyresources/Studymaterial.java
+++ StudyLink/src/model/Studyresources/Studymaterial.java
@@ -1,5 +1,42 @@
 package model.Studyresources;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public class Studymaterial {
-    // to be done
+    private String material_id;
+    private List<String> url;
+    private double rating ;
+
+    public Studymaterial(String material_id, List<String> url) throws SQLException {
+        this.material_id = material_id;
+        this.url = url;
+        this.rating = StudymaterialDAO.getRating(material_id);
+    }
+
+
+
+    public String getStudy_material_id() {
+        return material_id;
+    }
+
+    public List<String> getUrl() {
+        return url;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setmaterial_id(String material_id) {
+        this.material_id = material_id;
+    }
+
+    public void setUrl(List<String> url) {
+        this.url = url;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 }
--- StudyLink/src/model/Studyresources/StudymaterialDAO.java
+++ StudyLink/src/model/Studyresources/StudymaterialDAO.java
@@ -1,37 +1,55 @@
 package model.Studyresources;
 
 import model.Database.DatabaseConnection;
+import model.Topic.Topic;
 
 import javax.swing.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudymaterialDAO {
     static Connection connection = DatabaseConnection.getConnection();
-    public void setmaterialRating(String username,String Study_material_id,int rating) throws SQLException {
-        if( rating >= 1 && rating <= 5) {
-            String query = "insert into course_ratings (Study_material_id, username, rating) values (?, ?, ?) ON DUPLICATE KEY UPDATE rating = ?";
+
+    public static void setmaterialRating(String username, String material_id, int rating) throws SQLException {
+        if (rating < 1 || rating > 5){
+            return;
+        }
+            String query = "insert into Study_materials_ratings (material_id, username, rating) values (?, ?, ?) ON DUPLICATE KEY UPDATE rating = ?";
             PreparedStatement ps = connection.prepareStatement(query);
-            ps.setString(1, Study_material_id);
+            ps.setString(1, material_id);
             ps.setString(2, username);
             ps.setInt(3, rating);
             ps.setInt(4, rating);
             ps.executeUpdate();
-        }
-        // add joption pane for else statement
     }
-    public static double getRating(String Study_material_id) throws SQLException{
+
+    public static double getRating(String material_id) throws SQLException {
         double averageRating = 0;
-      String query =  "SELECT AVG(rating) AS AverageRating FROM Study_materials_ratings where Study_material_id = ?" ;
+        String query = "SELECT AVG(rating) AS AverageRating FROM Study_materials_ratings where material_id = ?";
         PreparedStatement ps = connection.prepareStatement(query);
-        ps.setString(1, Study_material_id);
+        ps.setString(1, material_id);
         ResultSet rs = ps.executeQuery();
         while (rs.next()) {
-
-             averageRating = rs.getDouble("AverageRating");
+            averageRating = rs.getDouble("AverageRating");
         }
         return averageRating;
+    }
+
+    public static Studymaterial getStudymaterial(String topicId) throws SQLException {
+        Studymaterial studymaterial = null;
+        List<String> url = new ArrayList<>();
+        String query = "select study_materials.material_id,study_materials_urls.url from study_materials JOIN  study_materials_urls ON study_materials.material_id = study_materials_urls.material_id  WHERE study_materials.topic_id = ?";
+        PreparedStatement ps = connection.prepareStatement(query);
+        ps.setString(1,topicId);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            url.add(rs.getString("url"));
+             studymaterial = new Studymaterial(rs.getString("material_id"),url);
+        }
+        return studymaterial;
     }
 }
