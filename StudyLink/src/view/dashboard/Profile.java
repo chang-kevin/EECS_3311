6a1f7e9e1eafa2185055d2366a0ad5a2d06a40e6
manalo-angela <77253494+manalo-angela@users.noreply.github.com>
2023-04-07 18:09:17 -0400

    Fixed UI bugs (#198)

--- StudyLink/src/view/dashboard/Profile.java
+++ StudyLink/src/view/dashboard/Profile.java
@@ -1,21 +1,32 @@
 package view.dashboard;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.sql.SQLException;
 
 import javax.swing.*;
 
+import model.User.User;
+import model.User.UserDAO;
+import model.User.UserSession;
 import net.miginfocom.swing.MigLayout;
 
 public class Profile extends RoundedPanel {
 
 
+    private User sessionUser;
+    private UserDAO userDAO;
     private JLabel profileImagePanel;
-    private JButton profileBtn;
+    private User user;
+    private JButton update;
 
-    public Profile() {
+    public Profile() throws SQLException {
         super(30, 30);
-
+        userDAO = new UserDAO();
+        sessionUser = UserSession.getInstance().getCurrentUser();
+        user = userDAO.getUser(sessionUser.getUsername());
 
         setBackground(new Color(232, 240, 238));
         setLayout(new MigLayout("fill", "", "20[30%!]15[10%!]5[10%!]20"));
@@ -49,20 +60,17 @@ public class Profile extends RoundedPanel {
     }
 
     public void setName() {
-        //JLabel userName = new JLabel(user.getFirstName() + " " + user.getLastName());
-        JLabel username = new JLabel("angela manalo");
+        JLabel username = new JLabel(user.getFirstName() + " " + user.getLastName());
         username.setForeground(new Color(128, 128, 128));
         username.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 13));
         username.setBorder(null);
         username.setHorizontalAlignment(SwingConstants.CENTER);
         add(username, "cell 0 1 1 1, grow");
-
     }
 
 
     public void viewRole() {
-        //role = new JLabel(user.getRole());
-        JLabel role = new JLabel("role");
+        JLabel role = new JLabel(user.getRole());
         role.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 13));
         role.setForeground(new Color(53, 79, 82));
         role.setBorder(null);
@@ -70,11 +78,11 @@ public class Profile extends RoundedPanel {
         add(role, "cell 0 2 1 1, grow");
     }
 
-
-
-
-
-
-
+    public void update() {
+        invalidate();
+        setName();
+        revalidate();
+        repaint();
+    }
 
 }
